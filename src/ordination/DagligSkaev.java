09e0ab3420614b9a9c3bfe160be4d72f3f8de4cb
MyDadCodedARobotToHitMe <72317990+MyDadCodedARobotToHitMe@users.noreply.github.com>
2025-03-13 09:52:46 +0100

    Daglig skæv

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,36 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination{
+    private ArrayList<Dosis> doser = new ArrayList<>();
 
 
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        doser.add(new Dosis(tid, antal));
+    }
+
+
+    @Override
+    public double samletDosis() {
+        double result = 0;
+        for (Dosis d : doser){
+            result += d.getAntal();
+        }
+        return result;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return samletDosis() / ChronoUnit.DAYS.between(getStartDen(), getSlutDen());
+    }
+
+    @Override
+    public String getType() {
+        return "Skæv";
     }
 }
