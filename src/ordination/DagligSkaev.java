2db86522ce2a5748427356893832cef25344486a
MyDadCodedARobotToHitMe <72317990+MyDadCodedARobotToHitMe@users.noreply.github.com>
2025-03-14 11:20:38 +0100

    controller metode skævord

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -27,7 +27,7 @@ public class DagligSkaev extends Ordination{
         for (Dosis d : doser){
             result += d.getAntal();
         }
-        return result;
+        return result * antalDage();
     }
 
     @Override
